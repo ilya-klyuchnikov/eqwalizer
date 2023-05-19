7758646ebcee5c0d1d3fb91ce9793c3c5ce43f78
Ilya Klyuchnikov <ilya.klyuchnikov@gmail.com>
2023-05-19 10:04:32 +0100

    prepare OTP 26 testing

---
+++ eqwalizer/test_projects/misc/src/otp26.erl
@@ -0,0 +1,8 @@
+%%% Copyright (c) Meta Platforms, Inc. and affiliates. All rights reserved.
+%%%
+%%% This source code is licensed under the Apache 2.0 license found in
+%%% the LICENSE file in the root directory of this source tree.
+
+-module(otp26).
+-feature(maybe_expr, enable).
+-compile([export_all, nowarn_export_all]).
